85adcbafc1cae0fdfa6913d95a759d4ee8dfbd66
Mr Raihan <36988513+raic0d3r@users.noreply.github.com>
2022-06-19 19:13:33 +0200

    Update SQLiVulnerableScanner.py

--- SQLiVulnerableScanner.py
+++ SQLiVulnerableScanner.py
@@ -1,10 +1,15 @@
 import re, requests, os, sys
 import html, urllib.request, urllib.error
+import socket, socks, urllib.parse 
 from time import time as timer  
 from multiprocessing.dummy import Pool
 from pathlib import Path
 from colorama import Fore                               
 from colorama import Style
+from stem import Signal
+from stem.control import Controller
+from urllib import parse
+
 #from web import web
 ####### Colors   ###### 
 fr  =   Fore.RED                                            
@@ -82,92 +87,34 @@ def check(html):
     return False, None
     
 def getoption():
-    print("{}[1]{} Single Site".format(fg, fw))
-    print("{}[2]{} Multiple Site".format(fg, fw))
-    choiceoption=input('Put Number => ')
-    if choiceoption=='1':
-        url = input("\n\033[92m[!]\033[91m ENTER WEBSITE : ")
-        start = timer()
-        singlescan(url)
-        print('Finished in : ' + str(timer() - start) + ' seconds')
-        print("\n")
-        if os.stat("logs/sqli_paramaters").st_size == 0:
-            print("\n")
-        else:
-            print("{}[1]{} Advanced Scan".format(fg, fw))
-            print("{}[2]{} No Thanks".format(fg, fw))
-            choiceoptions=input('Put Number => ')
-            if choiceoptions=='1':
-                print("{}{}Checking if the entry points are vulnerable...{}\n\n" .format(sn, fc, sn))
-                os.system("sqlmap -m logs/sqli_paramaters.txt -v 3 --batch --random-agent --level 5 --risk 3 | tee -a sqli.txt")
-            elif choiceoptions=='2':
-                exit()
+    url = input("\n\033[92m[!]\033[91m ENTER WEBSITE (Ex: evil.com): ")
+    start = timer()
+    singlescan(url)
+    print('Finished in : ' + str(timer() - start) + ' seconds')
+    print("\n")
 
-    elif choiceoption=='2':
-        start_raw = input("\n\033[92m[!]\033[91m ENTER LIST OF WEBSITES : ")
-        try:
-            with open(start_raw, 'r') as f:
-                url = f.read().splitlines()
-        except IOError:
-            pass
-        start = timer()
-        ThreadPool = Pool(100)
-        Threads = ThreadPool.map(multiplescan, url)
-        print('PrivateBot Finished in : ' + str(timer() - start) + ' seconds')
-        print("\n")
-        if os.stat("logs/sqli_paramaters").st_size == 0:
-            print("\n")
-        else:
-            print("{}[1]{} Advanced Scan".format(fg, fw))
-            print("{}[2]{} No Thanks".format(fg, fw))
-            choiceoptions=input('Put Number => ')
-            if choiceoptions=='1':
-                print("{}{}Checking if the entry points are vulnerable...{}\n\n" .format(sn, fc, sn))
-                os.system("sqlmap -m logs/sqli_paramaters.txt -v 3 --batch --random-agent --level 5 --risk 3 | tee -a sqli.txt")
-            elif choiceoptions=='2':
-                exit()
 
 def singlescan(url):
     print("\n{}[+] Vulnerability: SQLi{}\n".format(fg, sn))
     print("{}{}Finding SQLi entry points in the domain...{}\n\n".format(sn, fc, sn))
-    os.system("sudo gau "+url+"| gf sqli | tee logs/sqli_paramaters")
-    print("\n")
-    if os.stat("logs/sqli_paramaters").st_size == 0:
-        print("{}{}[😞]Not Found SQLi entry points in the domain...{}\n\n" .format(sn, fc, sn))
-    else:
-        print("{}{}[😍]Checking if the entry points are vulnerable...{}\n\n" .format(sn, fc, sn))
-        try:
-            with open("logs/sqli_paramaters", 'r') as f:
-                domain = f.read().splitlines()
-        except IOError:
-            pass
-        start = timer()
-        ThreadPool = Pool(100)
-        Threads = ThreadPool.map(scanner, domain)
+    Req = urllib.request.urlopen('http://web.archive.org/cdx/search/cdx?url=*.'+url+'/*&output=html&fl=original&collapse=urlkey', timeout=10).read().decode("utf8").splitlines()
+    for url in Req:
+        if re.search('(.*?)(.php\?|.asp\?|.apsx\?|.jsp\?)(.*?)=(.*?)', url):
+            print("{}{}[😍]Checking if the entry points are vulnerable...{}\n\n" .format(sn, fc, sn))
+            #print("{}{}[]==> {}{}\n\n" .format(sn, fc, url, sn))
+            scanner(url)
+            #Threads = ThreadPool.map(scanner, url)
+        else:
+            print("{}{}[😞]Not Found SQLi entry points in the domain...{}\n\n" .format(sn, fc, sn))
 
-def multiplescan(url):
-    print("\n{}[+] Vulnerability: SQLi{}\n".format(fg, sn))
-    print("{}{}Finding SQLi entry points in the domain...{}\n\n".format(sn, fc, sn))
-    os.system("sudo gau "+url+"| gf sqli | tee logs/sqli_paramaters")
-    print("\n")
-    if os.stat("logs/sqli_paramaters").st_size == 0:
-        print("{}{}[😞]Not Found SQLi entry points in the domain...{}\n\n" .format(sn, fc, sn))
-    else:
-        print("{}{}[😍]Checking if the entry points are vulnerable...{}\n\n" .format(sn, fc, sn))
-        try:
-            with open("logs/sqli_paramaters", 'r') as f:
-                domain = f.read().splitlines()
-        except IOError:
-            pass
-        start = timer()
-        ThreadPool = Pool(100)
-        Threads = ThreadPool.map(scanner, domain)
 
-def scanner(domain):
+
+def scanner(url):
     try:
+        print("{}{}[]==> {}{}\n\n" .format(sn, fc, url, sn))
         payloads = ("'", "')", "';", '"', '")', '";', '`', '`)', '`;', '\\', "%27", "%%2727", "%25%27", "%60", "%5C")
         for payload in payloads:
-            website = domain + payload
+            website = url + payload
             source = urllib.request.urlopen(website).read()
             mystr = source.decode("utf8")
             #source = html.unescape(r.text)
@@ -189,24 +136,6 @@ def scanner(domain):
     except:
         pass
 
-def installreq():
-    print("\n{}[+] Installing Requirement{}\n".format(fg, sn))
-    os.system("sudo su")
-    os.system("go install github.com/tomnomnom/gf@latest && go install github.com/lc/gau@latest")
-    os.system("cp ~/go/bin/gau /usr/bin && cp ~/go/bin/gf /usr/bin/")
-    print("\n{}Installed Done{}\n".format(fg, sn))
-    os.system("git clone https://github.com/Sherlock297/gf_patterns.git && cd gf_patterns/ && cp *.json ~/.gf")
-    getoption()
-
 banners()
-path_to_file = '/usr/bin/gau'
-path_to_file1 = '/usr/bin/gf'
-path = Path(path_to_file)
-path1 = Path(path_to_file1)
+getoption()
 
-if path.is_file() & path1.is_file():
-    print(f'The file {path_to_file} & {path_to_file1} exists')
-    getoption()
-else:
-    print(f'The file {path_to_file} & {path_to_file1} does not exist')
-    installreq()
